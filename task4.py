c5960d450ba44d36a504a7ec6036bd1b943b7cb0
kanybek740 <kanybekk1992@gmail.com>
2019-12-21 13:24:06 +0600

    Chapter1_Part2_Task4

---
+++ task4.py
@@ -0,0 +1,7 @@
+num_hours1 = int(input())
+num_min1 = int(input())
+num_sec1 = int(input())
+num_hours2 = int(input())
+num_min2 = int(input())
+num_sec2 = int(input())
+print(((num_hours2-num_hours1)*3600)+((num_min2-num_min1)*60)+(num_sec2-num_sec1))
